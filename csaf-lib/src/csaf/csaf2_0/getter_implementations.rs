57107aeececec5ad5b41e30e8ac62459e28391ee
Michael Lux <michael.lux@aisec.fraunhofer.de>
2025-02-28 14:12:46 +0100

    Add detailed documentation for CSAF-related traits and methods
    Detailed explanations and doc comments were added to traits and methods to clarify their roles in the CSAF document structure. This improves code readability and provides guidance on key functionalities, such as remediation category normalization and product resolution mechanisms.

--- csaf-lib/src/csaf/csaf2_0/getter_implementations.rs
+++ csaf-lib/src/csaf/csaf2_0/getter_implementations.rs
@@ -5,8 +5,25 @@ use std::ops::Deref;
 use std::str::FromStr;
 
 impl RemediationTrait for Remediation {
+
+    /// Normalizes the remediation categories from CSAF 2.0 to those of CSAF 2.1.
+    ///
+    /// # Explanation
+    /// In CSAF 2.1, the list of remediation categories was expanded, making it a superset of those
+    /// in CSAF 2.0. This function ensures that the remediation category from a CSAF 2.0 remediation
+    /// object is converted into the corresponding category defined in CSAF 2.1. Due to the
+    /// superset relationship between the category sets, this conversion can safely be performed
+    /// without failures.
+    ///
+    /// # Panics
+    /// This function assumes that every CSAF 2.0 category is valid within the CSAF 2.1 superset.
+    /// If, for any reason, `from_str` fails to match a valid category, the function will panic.
+    /// However, this situation should never occur due to the aforementioned superset relationship.
+    ///
+    /// # Returns
+    /// A CSAF 2.1 `CategoryOfTheRemediation` that corresponds to the remediation category of the
+    /// current object.
     fn get_category(&self) -> Remediation21 {
-        // Categories are identical, so this should never fail
         Remediation21::from_str(self.category.to_string().as_str()).unwrap()
     }
 

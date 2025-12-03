5c32bef59bbd4c03c5df7be7aabcd2a76ec2984f
Johannes Pein <johannes.pein@xitaso.com>
2025-12-03 13:16:06 +0100

    fix: use stable sort

--- csaf-rs/src/version_helpers.rs
+++ csaf-rs/src/version_helpers.rs
@@ -45,7 +45,7 @@ pub fn generate_revision_history_tuples(doc: &impl CsafTrait) -> Vec<RevisionHis
 
 /// Sorts the revision history tuples first by date, second by number
 pub fn sort_revision_history_tuples_by_date_by_number(tuples: &mut Vec<RevisionHistoryTupleType>) {
-    tuples.sort_unstable_by_key(|item| (item.1, item.2.clone()));
+    tuples.sort_by_key(|item| (item.1, item.2.clone()));
 }
 
 /// Sorts the revision history tuples by number

c3602cabac1b574f5818520789653a3c47cb170e
Thomas Ziemek <thomas.ziemek@xitaso.com>
2025-12-03 09:17:01 +0100

    extract function to evaluate cvss

--- csaf-rs/src/validations/test_6_1_08.rs
+++ csaf-rs/src/validations/test_6_1_08.rs
@@ -1,4 +1,5 @@
-use serde_json::Value;
+use jsonschema::Validator;
+use serde_json::{Map, Value};
 
 use crate::{
     csaf_traits::{
@@ -10,21 +11,11 @@ use crate::{
 /// 6.1.8 Invalid CVSS
 /// Invalid CVSS object according to scheme
 pub fn test_6_1_08_invalid_cvss(doc: &impl CsafTrait) -> Result<(), Vec<ValidationError>> {
-    let cvss20_schema_str = include_str!("../../assets/cvss-v2.0.json");
-    let parsed_cvss20_schema: Value = serde_json::from_str(cvss20_schema_str).unwrap();
-    let cvss20_validator = jsonschema::validator_for(&parsed_cvss20_schema).unwrap();
 
-    let cvss30_schema_str = include_str!("../../assets/cvss-v3.0.json");
-    let parsed_cvss30_schema: Value = serde_json::from_str(cvss30_schema_str).unwrap();
-    let cvss30_validator = jsonschema::validator_for(&parsed_cvss30_schema).unwrap();
-
-    let cvss31_schema_str = include_str!("../../assets/cvss-v3.1.json");
-    let parsed_cvss31_schema: Value = serde_json::from_str(cvss31_schema_str).unwrap();
-    let cvss31_validator = jsonschema::validator_for(&parsed_cvss31_schema).unwrap();
-
-    let cvss40_schema_str = include_str!("../../assets/cvss-v4.0.json");
-    let parsed_cvss40_schema: Value = serde_json::from_str(cvss40_schema_str).unwrap();
-    let cvss40_validator = jsonschema::validator_for(&parsed_cvss40_schema).unwrap();
+    let cvss20_validator = create_validator(include_str!("../../assets/cvss-v2.0.json"));
+    let cvss30_validator = create_validator(include_str!("../../assets/cvss-v3.0.json"));
+    let cvss31_validator = create_validator(include_str!("../../assets/cvss-v3.1.json"));
+    let cvss40_validator = create_validator(include_str!("../../assets/cvss-v4.0.json"));
 
     let mut errors: Vec<ValidationError> = Vec::new();
 
@@ -32,73 +23,52 @@ pub fn test_6_1_08_invalid_cvss(doc: &impl CsafTrait) -> Result<(), Vec<Validati
         if let Some(metrics) = vulnerability.get_metrics() {
             for (metric_index, metric) in metrics.iter().enumerate() {
                 let content = metric.get_content();
+                let instance_prefix = content.get_content_json_path(i_v, metric_index);
                 if let Some(cvss2) = content.get_cvss_v2() {
-                    let value = serde_json::to_value(cvss2).unwrap();
-                    let prop_name = get_metric_prop_name(VulnerabilityMetric::CvssV2);
-                    let evaluation = cvss20_validator.evaluate(&value);
-                    for error in evaluation.iter_errors() {
-                        errors.push(ValidationError {
-                            message: error.error.to_string(),
-                            instance_path: format!(
-                                "{}/{}",
-                                content.get_content_json_path(i_v, metric_index),
-                                prop_name
-                            ),
-                        });
-                    }
+                    let instance_path = create_instance_path(&instance_prefix, VulnerabilityMetric::CvssV2);
+                    evaluate_cvss(cvss2, &cvss20_validator, instance_path.clone(), &mut errors);
                 }
                 if let Some(cvss3) = content.get_cvss_v3() {
+                    // Use as_str because otherwise additional quotation marks would be included
                     if let Some(version) = cvss3.get("version").and_then(|v| v.as_str()) {
-                        // Use as_str because otherwise additional quotation marks would be included
-                        let value = serde_json::to_value(cvss3).unwrap();
-                        let prop_name = get_metric_prop_name(VulnerabilityMetric::CvssV3(version.to_string()));
+                        let instance_path = create_instance_path(&instance_prefix, VulnerabilityMetric::CvssV3(version.to_string()));
                         if version == "3.0" {
-                            let evaluation = cvss30_validator.evaluate(&value);
-                            for error in evaluation.iter_errors() {
-                                errors.push(ValidationError {
-                                    message: error.error.to_string(),
-                                    instance_path: format!(
-                                        "{}/{}",
-                                        content.get_content_json_path(i_v, metric_index),
-                                        prop_name
-                                    ),
-                                });
-                            }
+                            evaluate_cvss(cvss3, &cvss30_validator, instance_path.clone(), &mut errors);
                         } else if version == "3.1" {
-                            let evaluation = cvss31_validator.evaluate(&value);
-                            for error in evaluation.iter_errors() {
-                                errors.push(ValidationError {
-                                    message: error.error.to_string(),
-                                    instance_path: format!(
-                                        "{}/{}",
-                                        content.get_content_json_path(i_v, metric_index),
-                                        prop_name
-                                    ),
-                                });
-                            }
+                            evaluate_cvss(cvss3, &cvss31_validator, instance_path.clone(), &mut errors);
                         }
                     }
                 }
                 if let Some(cvss4) = content.get_cvss_v4() {
-                    let value = serde_json::to_value(cvss4).unwrap();
-                    let evaluation = cvss40_validator.evaluate(&value);
-                    let prop_name = get_metric_prop_name(VulnerabilityMetric::CvssV4);
-                    for error in evaluation.iter_errors() {
-                        errors.push(ValidationError {
-                            message: error.error.to_string(),
-                            instance_path: format!(
-                                "{}/{}",
-                                content.get_content_json_path(i_v, metric_index),
-                                prop_name
-                            ),
-                        });
-                    }
+                    let instance_path = create_instance_path(&instance_prefix, VulnerabilityMetric::CvssV4);
+                    evaluate_cvss(cvss4, &cvss40_validator, instance_path.clone(), &mut errors);
                 }
             }
         }
     }
 
     if errors.is_empty() { Ok(()) } else { Err(errors) }
+}
+
+fn create_validator(schema_str: &str) -> Validator {
+    let parsed_schema: Value = serde_json::from_str(schema_str).unwrap();
+    jsonschema::validator_for(&parsed_schema).unwrap()
+}
+
+fn create_instance_path(base: &str, metric: VulnerabilityMetric) -> String {
+    let prop_name = get_metric_prop_name(metric);
+    format!("{}/{}", base, prop_name)
+}
+
+fn evaluate_cvss (cvss_value: &Map<String, Value>, validator: &Validator, instance_path: String, errors: &mut Vec<ValidationError>) {
+    let value = serde_json::to_value(cvss_value).unwrap();
+    let evaluation = validator.evaluate(&value);
+    for error in evaluation.iter_errors() {
+        errors.push(ValidationError {
+            message: error.error.to_string(),
+            instance_path: instance_path.clone(),
+        });
+    }
 }
 
 #[cfg(test)]
@@ -137,6 +107,7 @@ mod tests {
                 ),
             ]),
         );
+        // 2.1 tests are not valid at the moment
         /*
         run_csaf21_tests(
             "08",

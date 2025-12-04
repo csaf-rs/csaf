23e15c30c6850651a7bc6b8771c3f2dc1ba9435b
Johannes Pein <johannes.pein@xitaso.com>
2025-12-04 19:33:40 +0100

    feat: add 6.1.27.11

---
+++ csaf-rs/src/validations/test_6_1_27_11.rs
@@ -0,0 +1,73 @@
+use crate::csaf_traits::{CsafTrait, CsafVersion, DocumentCategory, DocumentTrait, VulnerabilityTrait};
+use crate::validation::ValidationError;
+
+/// 6.1.27.11 Vulnerabilities
+///
+/// This test only applies to documents with `/document/category` with value `csaf_security_advisory` and `csaf_vex` for
+/// `/document/csaf_version` `2.0` and additionally to documents with `/document/category` with
+/// value `csaf_deprecated_security_advisory` for `/document/csaf_version` `2.1`.
+///
+/// In documents with this category a `/vulnerabilities[]` element must exist.
+pub fn test_6_1_27_11_vulnerabilities(doc: &impl CsafTrait) -> Result<(), Vec<ValidationError>> {
+    let doc_category = doc.get_document().get_category();
+
+    // check if document is relevant document category in csaf 2.0
+    if *doc.get_document().get_csaf_version() == CsafVersion::X20 {
+        if doc_category != DocumentCategory::CsafSecurityAdvisory && doc_category != DocumentCategory::CsafVex {
+            return Ok(());
+        }
+    }
+
+    // check if document is relevant document category in csaf 2.1
+    if *doc.get_document().get_csaf_version() == CsafVersion::X21 {
+        if doc_category != DocumentCategory::CsafSecurityAdvisory
+            && doc_category != DocumentCategory::CsafVex
+            && doc_category != DocumentCategory::CsafDeprecatedSecurityAdvisory
+        {
+            return Ok(());
+        }
+    }
+
+    if doc.get_vulnerabilities().is_empty() {
+        return Err(vec![test_6_1_27_11_err_generator(&doc_category)]);
+    }
+
+    Ok(())
+}
+
+fn test_6_1_27_11_err_generator(document_category: &DocumentCategory) -> ValidationError {
+    ValidationError {
+        message: format!(
+            "Document with category '{}' must not have a '/vulnerabilities' element",
+            document_category
+        ),
+        instance_path: "/vulnerabilities".to_string(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::csaf_traits::DocumentCategory;
+    use crate::test_helper::{run_csaf20_tests, run_csaf21_tests};
+    use crate::validations::test_6_1_27_11::{test_6_1_27_11_err_generator, test_6_1_27_11_vulnerabilities};
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_test_6_1_27_11() {
+        let errors = HashMap::from([
+            (
+                "01",
+                vec![test_6_1_27_11_err_generator(&DocumentCategory::CsafSecurityAdvisory)],
+            ),
+            ("02", vec![test_6_1_27_11_err_generator(&DocumentCategory::CsafVex)]),
+            (
+                "03",
+                vec![test_6_1_27_11_err_generator(
+                    &DocumentCategory::CsafDeprecatedSecurityAdvisory,
+                )],
+            ),
+        ]);
+        run_csaf20_tests("27-11", test_6_1_27_11_vulnerabilities, errors.clone());
+        run_csaf21_tests("27-11", test_6_1_27_11_vulnerabilities, errors);
+    }
+}

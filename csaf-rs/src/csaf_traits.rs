b9ff65564e81d0d5fb1ec348151d4649aec5089e
Johannes Pein <johannes.pein@xitaso.com>
2025-12-19 11:23:10 +0100

    fix: improve comment

--- csaf-rs/src/csaf_traits.rs
+++ csaf-rs/src/csaf_traits.rs
@@ -177,7 +177,7 @@ pub trait DocumentTrait {
 ///
 /// Contrary to other enums that are based on enums in the generated schemas, we are re-defining
 /// this enum in the trait. Each schema only contains an enum with "their" version, and merging them
-/// would be more complex then defining here and mapping in the implementations.
+/// would be more complex than defining them here and mapping to them in each implementation.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum CsafVersion {
     X20,

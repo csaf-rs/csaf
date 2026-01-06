7f9dc8000c450a083840743c4d522e6b7a387b5c
Johannes Pein <johannes.pein@xitaso.com>
2026-01-06 12:28:29 +0100

    feat: improve error message, generate one message for each offending path

--- csaf-rs/src/validations/test_6_1_33.rs
+++ csaf-rs/src/validations/test_6_1_33.rs
@@ -2,7 +2,7 @@ use crate::csaf_traits::{CsafTrait, FlagTrait, VulnerabilityTrait, WithOptionalG
 use crate::csaf2_1::schema::LabelOfTheFlag;
 use crate::helpers::resolve_product_groups;
 use crate::validation::ValidationError;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 /// 6.1.33 Multiple Flags with VEX Justification Codes per Product
 pub fn test_6_1_33_multiple_flags_with_vex_codes_per_product(doc: &impl CsafTrait) -> Result<(), Vec<ValidationError>> {
@@ -10,32 +10,33 @@ pub fn test_6_1_33_multiple_flags_with_vex_codes_per_product(doc: &impl CsafTrai
 
     // Check each flag in each vulnerability
     for (vuln_i, vulnerability) in doc.get_vulnerabilities().iter().enumerate() {
-        // Generate a hashmap of unique flag labels to product IDs in a hashset, which guarantees uniqueness of the product ids
-        let mut flag_to_product_ids_map: HashMap<LabelOfTheFlag, HashSet<String>> = HashMap::new();
-        // Generate a hashmap of product ids back to group ids and the flag labels for back-tracing in the error message
-        let mut product_id_to_group_id_map: HashMap<String, HashSet<String>> = HashMap::new();
+        // Generate a hashmap of product IDs to flag labels, flag indices, and group ids
+        let mut product_id_to_flags_map: HashMap<String, Vec<(LabelOfTheFlag, usize, Option<&String>)>> = HashMap::new();
         // if there are flags for the vuln, iterate over them
         if let Some(flags) = vulnerability.get_flags() {
-            for flag in flags.iter() {
-                // get entry or put empty entry for the flag in the flag -> product id map
-                let entry = flag_to_product_ids_map.entry(flag.get_label()).or_default();
+            for (flag_i, flag) in flags.iter().enumerate() {
+                let label = flag.get_label();
 
-                // add all product ids to the product id hashset
+                // add all product ids to the product_id_to_flags map (no group ids for direct product ids)
                 if let Some(product_ids) = flag.get_product_ids() {
-                    entry.extend(product_ids.map(|id| id.to_string()));
+                    for product_id in product_ids {
+                        product_id_to_flags_map
+                            .entry(product_id.to_string())
+                            .or_default()
+                            .push((label, flag_i, None));
+                    }
                 }
 
-                // iterate over all group ids, resolve each group id seperatly
+                // iterate over all group ids, resolve each group id separately
                 if let Some(group_ids) = flag.get_group_ids() {
                     for group_id in group_ids {
                         if let Some(resolved_product_ids) = resolve_product_groups(doc, [group_id].into_iter()) {
-                            // add the resolved group id to the product id hashset
-                            entry.extend(resolved_product_ids.clone().into_iter());
-                            // add product id -> group id + flag label mapping
+                            // add the resolved product ids to the product_id_to_flags map with group id
                             for product_id in resolved_product_ids {
-                                let product_to_group_id_entry =
-                                    product_id_to_group_id_map.entry(product_id).or_default();
-                                product_to_group_id_entry.insert(group_id.to_string());
+                                product_id_to_flags_map
+                                    .entry(product_id)
+                                    .or_default()
+                                    .push((label, flag_i, Some(group_id)));
                             }
                         }
                     }
@@ -43,24 +44,23 @@ pub fn test_6_1_33_multiple_flags_with_vex_codes_per_product(doc: &impl CsafTrai
             }
         }
 
-        // Invert the flag -> product id hashmap to map unique product IDs to flag labels
-        let mut product_id_to_flags: HashMap<String, Vec<LabelOfTheFlag>> = HashMap::new();
-        for (label, product_ids) in flag_to_product_ids_map {
-            for product_id in product_ids.iter() {
-                product_id_to_flags.entry(product_id.clone()).or_default().push(label);
-            }
-        }
-
         // Collect errors for products that appear in multiple flag labels
-        for (product_id, labels) in product_id_to_flags {
-            if labels.len() > 1 {
+        for (product_id, flag_flag_i_group_ids_arr) in product_id_to_flags_map {
+            // Extract unique labels from the (label, flag_i, group_ids) tuples
+            if flag_flag_i_group_ids_arr.len() > 1 {
+                // extract all labels
+                let labels: Vec<LabelOfTheFlag> = flag_flag_i_group_ids_arr.iter().map(|(label, _, _)| *label).collect();
                 // generate error
-                errors.get_or_insert_with(Vec::new).push(test_6_1_33_err_generator(
-                    &product_id,
-                    &labels,
-                    product_id_to_group_id_map.get(&product_id),
-                    vuln_i,
-                ));
+                for (label, flag_i, group_id) in flag_flag_i_group_ids_arr {
+                    errors.get_or_insert_with(Vec::new).push(test_6_1_33_err_generator(
+                        &product_id,
+                        &labels,
+                        label,
+                        group_id,
+                        vuln_i,
+                        flag_i
+                    ));
+                }
             }
         }
     }
@@ -71,23 +71,29 @@ pub fn test_6_1_33_multiple_flags_with_vex_codes_per_product(doc: &impl CsafTrai
 fn test_6_1_33_err_generator(
     product_id: &str,
     labels: &[LabelOfTheFlag],
-    group_ids: Option<&HashSet<String>>,
+    label: LabelOfTheFlag,
+    group_id: Option<&String>,
     vuln_i: usize,
+    flag_i: usize
 ) -> ValidationError {
     let labels_joined = {
         let mut labels_str: Vec<_> = labels.iter().map(|l| l.to_string()).collect();
         labels_str.sort();
         labels_str.join(", ")
     };
-    let group_ids_joined = group_ids
-        .map(|ids| format!(", via groups: {}", ids.iter().cloned().collect::<Vec<_>>().join(", ")))
-        .unwrap_or_default();
+    let group_id_str = {
+        if let Some(group_id) = group_id {
+            format!("(via group: {})", group_id)
+        } else {
+            "".to_string()
+        }
+    };
     ValidationError {
         message: format!(
-            "Product '{}' is associated with multiple flag labels: [{}], {}",
-            product_id, labels_joined, group_ids_joined
+            "Product '{}' is associated with multiple flag labels: [{}] {}, it has flag label {} on this path",
+            product_id, labels_joined, group_id_str, label
         ),
-        instance_path: format!("/vulnerabilities/{}/flags", vuln_i),
+        instance_path: format!("/vulnerabilities/{}/flags/{}", vuln_i, flag_i),
     }
 }
 
@@ -98,7 +104,7 @@ mod tests {
     use crate::validations::test_6_1_33::{
         test_6_1_33_err_generator, test_6_1_33_multiple_flags_with_vex_codes_per_product,
     };
-    use std::collections::{HashMap, HashSet};
+    use std::collections::HashMap;
 
     #[test]
     fn test_test_6_1_33() {
@@ -110,9 +116,22 @@ mod tests {
                     LabelOfTheFlag::ComponentNotPresent,
                     LabelOfTheFlag::VulnerableCodeCannotBeControlledByAdversary,
                 ],
-                Option::from(&HashSet::from(["CSAFGID-0001".to_string()])),
+                LabelOfTheFlag::ComponentNotPresent,
+                Option::from(&"CSAFGID-0001".to_string()),
                 0,
-            )],
+                0
+            ),
+                 test_6_1_33_err_generator(
+                     "CSAFPID-9080700",
+                     &[
+                         LabelOfTheFlag::ComponentNotPresent,
+                         LabelOfTheFlag::VulnerableCodeCannotBeControlledByAdversary,
+                     ],
+                     LabelOfTheFlag::VulnerableCodeCannotBeControlledByAdversary,
+                     None,
+                     0,
+                     1
+                 )],
         )]);
         run_csaf20_tests(
             "33",

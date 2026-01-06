6d5b9b66813e4fd806a09a6851590d9525ee7a65
Johannes Pein <johannes.pein@xitaso.com>
2026-01-06 12:34:39 +0100

    fix: fmt + clippy

--- csaf-rs/src/validations/test_6_1_33.rs
+++ csaf-rs/src/validations/test_6_1_33.rs
@@ -4,6 +4,7 @@ use crate::helpers::resolve_product_groups;
 use crate::validation::ValidationError;
 use std::collections::HashMap;
 
+type VexJustificationInfo = (LabelOfTheFlag, usize, Option<String>);
 /// 6.1.33 Multiple Flags with VEX Justification Codes per Product
 pub fn test_6_1_33_multiple_flags_with_vex_codes_per_product(doc: &impl CsafTrait) -> Result<(), Vec<ValidationError>> {
     let mut errors: Option<Vec<ValidationError>> = None;
@@ -11,7 +12,7 @@ pub fn test_6_1_33_multiple_flags_with_vex_codes_per_product(doc: &impl CsafTrai
     // Check each flag in each vulnerability
     for (vuln_i, vulnerability) in doc.get_vulnerabilities().iter().enumerate() {
         // Generate a hashmap of product IDs to flag labels, flag indices, and group ids
-        let mut product_id_to_flags_map: HashMap<String, Vec<(LabelOfTheFlag, usize, Option<&String>)>> = HashMap::new();
+        let mut product_id_to_flags_map: HashMap<String, Vec<VexJustificationInfo>> = HashMap::new();
         // if there are flags for the vuln, iterate over them
         if let Some(flags) = vulnerability.get_flags() {
             for (flag_i, flag) in flags.iter().enumerate() {
@@ -33,10 +34,11 @@ pub fn test_6_1_33_multiple_flags_with_vex_codes_per_product(doc: &impl CsafTrai
                         if let Some(resolved_product_ids) = resolve_product_groups(doc, [group_id].into_iter()) {
                             // add the resolved product ids to the product_id_to_flags map with group id
                             for product_id in resolved_product_ids {
-                                product_id_to_flags_map
-                                    .entry(product_id)
-                                    .or_default()
-                                    .push((label, flag_i, Some(group_id)));
+                                product_id_to_flags_map.entry(product_id).or_default().push((
+                                    label,
+                                    flag_i,
+                                    Some(group_id.to_string()),
+                                ));
                             }
                         }
                     }
@@ -49,7 +51,8 @@ pub fn test_6_1_33_multiple_flags_with_vex_codes_per_product(doc: &impl CsafTrai
             // Extract unique labels from the (label, flag_i, group_ids) tuples
             if flag_flag_i_group_ids_arr.len() > 1 {
                 // extract all labels
-                let labels: Vec<LabelOfTheFlag> = flag_flag_i_group_ids_arr.iter().map(|(label, _, _)| *label).collect();
+                let labels: Vec<LabelOfTheFlag> =
+                    flag_flag_i_group_ids_arr.iter().map(|(label, _, _)| *label).collect();
                 // generate error
                 for (label, flag_i, group_id) in flag_flag_i_group_ids_arr {
                     errors.get_or_insert_with(Vec::new).push(test_6_1_33_err_generator(
@@ -58,7 +61,7 @@ pub fn test_6_1_33_multiple_flags_with_vex_codes_per_product(doc: &impl CsafTrai
                         label,
                         group_id,
                         vuln_i,
-                        flag_i
+                        flag_i,
                     ));
                 }
             }
@@ -72,9 +75,9 @@ fn test_6_1_33_err_generator(
     product_id: &str,
     labels: &[LabelOfTheFlag],
     label: LabelOfTheFlag,
-    group_id: Option<&String>,
+    group_id: Option<String>,
     vuln_i: usize,
-    flag_i: usize
+    flag_i: usize,
 ) -> ValidationError {
     let labels_joined = {
         let mut labels_str: Vec<_> = labels.iter().map(|l| l.to_string()).collect();
@@ -110,28 +113,30 @@ mod tests {
     fn test_test_6_1_33() {
         let errors = HashMap::from([(
             "01",
-            vec![test_6_1_33_err_generator(
-                "CSAFPID-9080700",
-                &[
+            vec![
+                test_6_1_33_err_generator(
+                    "CSAFPID-9080700",
+                    &[
+                        LabelOfTheFlag::ComponentNotPresent,
+                        LabelOfTheFlag::VulnerableCodeCannotBeControlledByAdversary,
+                    ],
                     LabelOfTheFlag::ComponentNotPresent,
+                    Some("CSAFGID-0001".to_string()),
+                    0,
+                    0,
+                ),
+                test_6_1_33_err_generator(
+                    "CSAFPID-9080700",
+                    &[
+                        LabelOfTheFlag::ComponentNotPresent,
+                        LabelOfTheFlag::VulnerableCodeCannotBeControlledByAdversary,
+                    ],
                     LabelOfTheFlag::VulnerableCodeCannotBeControlledByAdversary,
-                ],
-                LabelOfTheFlag::ComponentNotPresent,
-                Option::from(&"CSAFGID-0001".to_string()),
-                0,
-                0
-            ),
-                 test_6_1_33_err_generator(
-                     "CSAFPID-9080700",
-                     &[
-                         LabelOfTheFlag::ComponentNotPresent,
-                         LabelOfTheFlag::VulnerableCodeCannotBeControlledByAdversary,
-                     ],
-                     LabelOfTheFlag::VulnerableCodeCannotBeControlledByAdversary,
-                     None,
-                     0,
-                     1
-                 )],
+                    None,
+                    0,
+                    1,
+                ),
+            ],
         )]);
         run_csaf20_tests(
             "33",

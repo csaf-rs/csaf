21a14b138aa685afa1f95e5bdf23ddf9f946c31d
Johannes Pein <johannes.pein@xitaso.com>
2026-01-19 11:55:13 +0100

    fix: copilot comments

--- csaf-rs/src/csaf_traits.rs
+++ csaf-rs/src/csaf_traits.rs
@@ -1076,7 +1076,7 @@ pub trait ProductTreeTrait {
 
 /// Enum representing the category of a branch in a product tree.
 /// We need a shared type on the trait, as CSAF version 2.0 have fully divergent definitions.
-/// CSAF 2.0 has legaacy, which 2.1 has not.
+/// CSAF 2.0 has legacy, which 2.1 has not.
 /// CSAF 2.1 has platform, which 2.0 has not.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum CategoryOfTheBranch {

05d7408866d0cdd3556a6aaf534f5b03de330b65
Andreas Schierl <andreas.schierl@xitaso.com>
2026-02-11 15:08:14 +0100

    feat: add test 6.2.20

--- csaf-rs/src/validations/mod.rs
+++ csaf-rs/src/validations/mod.rs
@@ -78,7 +78,7 @@ pub mod test_6_2_16;
 pub mod test_6_2_17;
 pub mod test_6_2_18;
 // pub mod test_6_2_19;
-// pub mod test_6_2_20;
+pub mod test_6_2_20;
 
 pub mod test_6_3_1;
 // pub mod test_6_3_2;

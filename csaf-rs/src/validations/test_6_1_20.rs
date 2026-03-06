8201bdc49e3f67292acaabc1ee3412912d70dccd
Thomas Ziemek <thomas.ziemek@xitaso.com>
2026-03-06 08:19:17 +0100

    feat: add test 6.1.20 for CSAF 2.1 (#451)

--- csaf-rs/src/validations/test_6_1_20.rs
+++ csaf-rs/src/validations/test_6_1_20.rs
@@ -3,18 +3,18 @@ use crate::csaf_traits::{CsafTrait, DocumentTrait, TrackingTrait};
 use crate::schema::csaf2_1::schema::DocumentStatus;
 use crate::validation::ValidationError;
 
-fn create_validation_error(status: &DocumentStatus, version: &SemVerVersion) -> ValidationError {
+fn create_status_version_error(status: &DocumentStatus, version: &SemVerVersion) -> ValidationError {
     ValidationError {
         message: format!(
             "The document status is {status} but the document version {version} contains a pre-release part"
         ),
-        instance_path: "/document/version".to_string(),
+        instance_path: "/document/tracking/version".to_string(),
     }
 }
 
 /// 6.1.20 Non-draft Document Version
 ///
-/// For documents with status "final" or "interim", the `/document/version` field must not contain
+/// For documents with status "final" or "interim", the `/document/tracking/version` field must not contain
 /// a pre-release part (e.g. "1.0.0-alpha").
 pub fn test_6_1_20_non_draft_document_version(doc: &impl CsafTrait) -> Result<(), Vec<ValidationError>> {
     let tracking = doc.get_document().get_tracking();
@@ -22,21 +22,21 @@ pub fn test_6_1_20_non_draft_document_version(doc: &impl CsafTrait) -> Result<()
     // Check if the document status is not "final" or "interim"
     let status = tracking.get_status();
     if !(status == DocumentStatus::Final || status == DocumentStatus::Interim) {
-        return Ok(());
+        return Ok(()); // ToDo return skipped/not applicable
     }
 
     // Check if doc version is valid
     let doc_version = match tracking.get_version() {
         CsafVersionNumber::Valid(version_number) => version_number,
-        CsafVersionNumber::Invalid(err) => return Err(vec![err.get_validation_error("/document/version")]),
+        CsafVersionNumber::Invalid(err) => return Err(vec![err.get_validation_error("/document/tracking/version")]), // ToDo generate warning
     };
 
     match doc_version {
         // If version is integer versioning, this test does not apply
-        ValidVersionNumber::IntVer(_) => {},
+        ValidVersionNumber::IntVer(_) => {}, // ToDo maybe generate skipped/not applicable
         ValidVersionNumber::SemVer(semver) => {
             if semver.has_prerelease() {
-                return Err(vec![create_validation_error(&status, &semver)]);
+                return Err(vec![create_status_version_error(&status, &semver)]);
             }
         },
     }
@@ -78,14 +78,16 @@ mod tests {
 
     #[test]
     fn test_test_6_1_20() {
-        // TODO Unit tests for other Doc status
-        let case_01 = Err(vec![create_validation_error(
+        let case_interim = Err(vec![create_status_version_error(
             &DocumentStatus::Interim,
             &SemVerVersion::from(Version::from_str("1.0.0-alpha").unwrap()),
         )]);
+        let case_final = Err(vec![create_status_version_error(
+            &DocumentStatus::Final,
+            &SemVerVersion::from(Version::from_str("1.0.0-alpha").unwrap()),
+        )]);
 
-        // Both CSAF 2.0 and 2.1 have 1 test case
-        TESTS_2_0.test_6_1_20.expect(case_01.clone());
-        TESTS_2_1.test_6_1_20.expect(case_01);
+        TESTS_2_0.test_6_1_20.expect(case_interim.clone(), case_final.clone());
+        TESTS_2_1.test_6_1_20.expect(case_interim, case_final);
     }
 }

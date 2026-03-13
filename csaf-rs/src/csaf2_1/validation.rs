8970e991c18782763e6c670c72e48e9fe42a5e80
Andreas Schierl <andreas.schierl@xitaso.com>
2026-03-13 14:46:20 +0100

    feat: add test 6.1.54

--- csaf-rs/src/csaf2_1/validation.rs
+++ csaf-rs/src/csaf2_1/validation.rs
@@ -161,7 +161,7 @@ impl Validatable for CommonSecurityAdvisoryFramework {
                 "6.1.51" => None, // Some(ValidatorForTest6_1_51.validate(self)),
                 "6.1.52" => None, // Some(ValidatorForTest6_1_52.validate(self)),
                 "6.1.53" => None, // Some(ValidatorForTest6_1_53.validate(self)),
-                "6.1.54" => None, // Some(ValidatorForTest6_1_54.validate(self)),
+                "6.1.54" => Some(ValidatorForTest6_1_54.validate(self)),
                 "6.1.55" => None, // Some(ValidatorForTest6_1_55.validate(self)),
                 "6.1.56" => None, // Some(ValidatorForTest6_1_56.validate(self)),
                 _ => None,
--- csaf-rs/src/validations/mod.rs
+++ csaf-rs/src/validations/mod.rs
@@ -65,6 +65,7 @@ pub mod test_6_1_47;
 pub mod test_6_1_48;
 pub mod test_6_1_49;
 pub mod test_6_1_53;
+pub mod test_6_1_54;
 pub mod test_6_1_56;
 
 pub mod test_6_2_01;

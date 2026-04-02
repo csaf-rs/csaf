49935704b6d112d4cc17ec130d6707aac3a0a473
Thomas Ziemek <thomas.ziemek@xitaso.com>
2026-04-02 11:32:32 +0200

    chore: fmt

--- csaf-rs/src/csaf2_1/validation.rs
+++ csaf-rs/src/csaf2_1/validation.rs
@@ -166,7 +166,7 @@ impl Validatable for CommonSecurityAdvisoryFramework {
                 "6.1.56" => Some(ValidatorForTest6_1_56.validate(self)),
                 "6.1.57" => Some(ValidatorForTest6_1_57.validate(self)),
                 "6.1.58" => Some(ValidatorForTest6_1_58.validate(self)),
-                "6.1.59" => None, // Some(ValidatorForTest6_1_59.validate(self)),
+                "6.1.59" => None,   // Some(ValidatorForTest6_1_59.validate(self)),
                 "6.1.60.1" => None, // Some(ValidatorForTest6_1_60_1.validate(self)),
                 "6.1.60.2" => None, // Some(ValidatorForTest6_1_60_2.validate(self)),
                 "6.1.60.3" => None, // Some(ValidatorForTest6_1_60_3.validate(self)),

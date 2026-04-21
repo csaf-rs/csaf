d9b8b4b2c0bdfd36ad7f0d4960f2d63b31512bac
Thomas Ziemek <thomas.ziemek@xitaso.com>
2026-04-21 14:25:22 +0200

    removed more currently unnecessary code

--- csaf-ffi/src/types.rs
+++ csaf-ffi/src/types.rs
@@ -1,9 +1,5 @@
 //! FFI-friendly type definitions mirroring csaf-rs domain types.
 
-// ---------------------------------------------------------------------------
-// CSAF Version
-// ---------------------------------------------------------------------------
-
 #[derive(Debug, Clone, uniffi::Enum)]
 pub enum CsafVersion {
     V20,
@@ -18,10 +14,6 @@ impl From<&csaf::csaf::enums::csaf_version::CsafVersion> for CsafVersion {
         }
     }
 }
-
-// ---------------------------------------------------------------------------
-// Document Category
-// ---------------------------------------------------------------------------
 
 #[derive(Debug, Clone, uniffi::Enum)]
 pub enum DocumentCategory {
@@ -53,52 +45,6 @@ impl From<&csaf::csaf::types::csaf_document_category::CsafDocumentCategory> for 
     }
 }
 
-// ---------------------------------------------------------------------------
-// Category of Branch
-// ---------------------------------------------------------------------------
-
-#[derive(Debug, Clone, uniffi::Enum)]
-pub enum CategoryOfTheBranch {
-    Architecture,
-    HostName,
-    Language,
-    Legacy,
-    PatchLevel,
-    Platform,
-    ProductFamily,
-    ProductName,
-    ProductVersion,
-    ProductVersionRange,
-    ServicePack,
-    Specification,
-    Vendor,
-}
-
-impl From<&csaf::csaf::enums::category_of_the_branch::CategoryOfTheBranch> for CategoryOfTheBranch {
-    fn from(c: &csaf::csaf::enums::category_of_the_branch::CategoryOfTheBranch) -> Self {
-        use csaf::csaf::enums::category_of_the_branch::CategoryOfTheBranch as C;
-        match c {
-            C::Architecture => Self::Architecture,
-            C::HostName => Self::HostName,
-            C::Language => Self::Language,
-            C::Legacy => Self::Legacy,
-            C::PatchLevel => Self::PatchLevel,
-            C::Platform => Self::Platform,
-            C::ProductFamily => Self::ProductFamily,
-            C::ProductName => Self::ProductName,
-            C::ProductVersion => Self::ProductVersion,
-            C::ProductVersionRange => Self::ProductVersionRange,
-            C::ServicePack => Self::ServicePack,
-            C::Specification => Self::Specification,
-            C::Vendor => Self::Vendor,
-        }
-    }
-}
-
-// ---------------------------------------------------------------------------
-// Product Status Group
-// ---------------------------------------------------------------------------
-
 #[derive(Debug, Clone, uniffi::Enum)]
 pub enum ProductStatusGroup {
     Affected,
@@ -123,10 +69,6 @@ impl From<&csaf::csaf::enums::product_status_group::ProductStatusGroup> for Prod
     }
 }
 
-// ---------------------------------------------------------------------------
-// CWE
-// ---------------------------------------------------------------------------
-
 #[derive(Debug, Clone, uniffi::Record)]
 pub struct Cwe {
     pub id: String,
@@ -143,10 +85,6 @@ impl From<&csaf::csaf::types::csaf_cwe::Cwe> for Cwe {
         }
     }
 }
-
-// ---------------------------------------------------------------------------
-// DateTime
-// ---------------------------------------------------------------------------
 
 #[derive(Debug, Clone, uniffi::Enum)]
 pub enum CsafDateTime {
@@ -168,10 +106,6 @@ impl From<&csaf::csaf::types::csaf_datetime::CsafDateTime> for CsafDateTime {
     }
 }
 
-// ---------------------------------------------------------------------------
-// Language
-// ---------------------------------------------------------------------------
-
 #[derive(Debug, Clone, uniffi::Enum)]
 pub enum CsafLanguage {
     Valid { value: String },
@@ -188,38 +122,6 @@ impl From<&csaf::csaf::types::language::CsafLanguage> for CsafLanguage {
     }
 }
 
-// ---------------------------------------------------------------------------
-// Vulnerability Metric type
-// ---------------------------------------------------------------------------
-
-#[derive(Debug, Clone, uniffi::Enum)]
-pub enum VulnerabilityMetric {
-    SsvcV1,
-    CvssV2 { version: String },
-    CvssV3 { version: String },
-    CvssV4 { version: String },
-    Epss,
-    QualitativeSeverityRating,
-}
-
-impl From<&csaf::csaf::types::csaf_vuln_metric::CsafVulnerabilityMetric> for VulnerabilityMetric {
-    fn from(m: &csaf::csaf::types::csaf_vuln_metric::CsafVulnerabilityMetric) -> Self {
-        use csaf::csaf::types::csaf_vuln_metric::CsafVulnerabilityMetric as M;
-        match m {
-            M::SsvcV1 => Self::SsvcV1,
-            M::CvssV2(v) => Self::CvssV2 { version: v.clone() },
-            M::CvssV3(v) => Self::CvssV3 { version: v.clone() },
-            M::CvssV4(v) => Self::CvssV4 { version: v.clone() },
-            M::Epss => Self::Epss,
-            M::QualitativeSeverityRating => Self::QualitativeSeverityRating, // Not currently supported in the API, but could be added in the future if needed.
-        }
-    }
-}
-
-// ---------------------------------------------------------------------------
-// Product reference tuple (used throughout the API)
-// ---------------------------------------------------------------------------
-
 #[derive(Debug, Clone, uniffi::Record)]
 pub struct ProductReference {
     pub product_id: String,
@@ -230,24 +132,4 @@ impl From<(String, String)> for ProductReference {
     fn from((product_id, json_path): (String, String)) -> Self {
         Self { product_id, json_path }
     }
-}
-
-// ---------------------------------------------------------------------------
-// Note
-// ---------------------------------------------------------------------------
-
-#[derive(Debug, Clone, uniffi::Record)]
-pub struct Note {
-    pub category: String,
-    pub title: Option<String>,
-}
-
-// ---------------------------------------------------------------------------
-// Vulnerability ID
-// ---------------------------------------------------------------------------
-
-#[derive(Debug, Clone, uniffi::Record)]
-pub struct VulnerabilityId {
-    pub system_name: String,
-    pub text: String,
 }

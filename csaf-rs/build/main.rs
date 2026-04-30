3bcbad686d8b070422f7a97fbcd76944b71be823
Johannes Pein <johannes.pein@xitaso.com>
2026-04-30 16:04:31 +0200

    fix: cr comments

--- csaf-rs/build/main.rs
+++ csaf-rs/build/main.rs
@@ -1,6 +1,9 @@
 mod ssvc_validation;
 
 fn main() {
+    println!("cargo:rerun-if-changed=build/main.rs");
+    println!("cargo:rerun-if-changed=build/ssvc_validation.rs");
+
     ssvc_validation::validate_ssvc_lib_json();
 }
 

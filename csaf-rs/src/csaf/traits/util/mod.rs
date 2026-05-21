75833f893100c7353f8c46cea357f9a9029aebb9
Johannes Pein <johannes.pein@xitaso.com>
2026-05-21 23:29:35 +0200

    style: fmt

--- csaf-rs/src/csaf/traits/util/mod.rs
+++ csaf-rs/src/csaf/traits/util/mod.rs
@@ -4,6 +4,6 @@ pub mod not_present_20;
 pub mod resolve_product_groups;
 
 pub(crate) use generic_with::{
-    impl_with_date, impl_with_optional_date, impl_with_optional_group_ids,
-    impl_with_optional_product_ids, impl_without_group_ids, impl_without_product_ids,
+    impl_with_date, impl_with_optional_date, impl_with_optional_group_ids, impl_with_optional_product_ids,
+    impl_without_group_ids, impl_without_product_ids,
 };

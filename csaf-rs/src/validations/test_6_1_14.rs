4a6da23bb0088bee7cdd9581ef95a803e09401a6
Thomas Ziemek <thomas.ziemek@xitaso.com>
2026-06-30 11:55:34 +0200

    fix: refactor 6.1.14 for semantic version changes

--- csaf-rs/src/validations/test_6_1_14.rs
+++ csaf-rs/src/validations/test_6_1_14.rs
@@ -1,12 +1,10 @@
 use crate::csaf_traits::{CsafTrait, DocumentTrait, TrackingTrait};
 use crate::validation::ValidationError;
 
-fn create_revision_history_error(revision_number: impl std::fmt::Display, path_index: usize) -> ValidationError {
+fn create_revision_history_error() -> ValidationError {
     ValidationError {
-        message: format!(
-            "Revision history is not sorted by date, revision with number {revision_number} is out of place"
-        ),
-        instance_path: format!("/document/tracking/revision_history/{path_index}"),
+        message: "items must be in ascending order when sorted by `date` and `number`".to_string(),
+        instance_path: "/document/tracking/revision_history".to_string(),
     }
 }
 
@@ -14,6 +12,7 @@ fn create_revision_history_error(revision_number: impl std::fmt::Display, path_i
 ///
 /// The revision history items, when sorted by their `/document/tracking/revision_history[]/date` field,
 /// must be in the same order as when sorted by their `/document/tracking/revision_history[]/number` field.
+/// If the version numbers are mixed between semantic versioning and non-semantic versioning, the non-semantic versioning numbers are interpreted as semantic versioning numbers.
 pub fn test_6_1_14_sorted_revision_history(doc: &impl CsafTrait) -> Result<(), Vec<ValidationError>> {
     // Generate tuples of (revision history path index, date, number)
     let mut rev_history_tuples_sort_by_date = doc.get_document().get_tracking().aggregate_revision_history();
@@ -30,7 +29,8 @@ pub fn test_6_1_14_sorted_revision_history(doc: &impl CsafTrait) -> Result<(), V
             && let Some(by_number) = rev_history_tuples_sort_by_number.get(i)
         {
             if by_date.date != by_number.date {
-                errors.push(create_revision_history_error(&by_date.number, by_date.path_index));
+                errors.push(create_revision_history_error());
+                break;
             }
         } else {
             unreachable!("Both arrays should have same length, this looks like a dev error.")
@@ -55,49 +55,18 @@ mod tests {
     #[test]
     fn test_test_6_1_14() {
         // Error cases
-        let case_01 = Err(vec![
-            create_revision_history_error("2", 0),
-            create_revision_history_error("1", 1),
-        ]);
-        let case_02 = Err(vec![
-            create_revision_history_error("2", 0),
-            create_revision_history_error("1", 1),
-        ]);
-        let case_03 = Err(vec![
-            create_revision_history_error("2", 1),
-            create_revision_history_error("1", 0),
-        ]);
-        let case_04 = Err(vec![
-            create_revision_history_error("2.0.0", 0),
-            create_revision_history_error("1.0.0", 1),
-        ]);
-        let case_05 = Err(vec![
-            create_revision_history_error("2.0.0", 0),
-            create_revision_history_error("1.0.0", 1),
-        ]);
-        let case_06 = Err(vec![
-            create_revision_history_error("10", 9),
-            create_revision_history_error("9", 8),
-        ]);
-        let case_07 = Err(vec![
-            create_revision_history_error("1.10.0", 10),
-            create_revision_history_error("1.9.0", 9),
-        ]);
-        let case_08 = Err(vec![
-            create_revision_history_error("2", 1),
-            create_revision_history_error("1", 0),
-        ]);
+        let case_error = Err(vec![create_revision_history_error()]);
 
         // CSAF 2.0 has 17 test cases (01-08, 11-19)
         TESTS_2_0.test_6_1_14.expect(
-            case_01.clone(),
-            case_02.clone(),
-            case_03.clone(),
-            case_04.clone(),
-            case_05.clone(),
-            case_06.clone(),
-            case_07.clone(),
-            case_08.clone(),
+            case_error.clone(),
+            case_error.clone(),
+            case_error.clone(),
+            case_error.clone(),
+            case_error.clone(),
+            case_error.clone(),
+            case_error.clone(),
+            case_error.clone(),
             Ok(()), // case_11
             Ok(()), // case_12
             Ok(()), // case_13
@@ -107,22 +76,20 @@ mod tests {
             Ok(()), // case_17
             Ok(()), // case_18
             Ok(()), // case_19
+            Ok(()), // supplementary case s11 mixed versioning
         );
 
         // CSAF 2.1 has 19 test cases (01-09, 11-19, 31)
         TESTS_2_1.test_6_1_14.expect(
-            case_01,
-            case_02,
-            case_03,
-            case_04,
-            case_05,
-            case_06,
-            case_07,
-            case_08,
-            Err(vec![
-                create_revision_history_error("2", 0),
-                create_revision_history_error("1", 1),
-            ]),
+            case_error.clone(),
+            case_error.clone(),
+            case_error.clone(),
+            case_error.clone(),
+            case_error.clone(),
+            case_error.clone(),
+            case_error.clone(),
+            case_error.clone(),
+            case_error,
             Ok(()), // case_11
             Ok(()), // case_12
             Ok(()), // case_13
@@ -133,6 +100,7 @@ mod tests {
             Ok(()), // case_18
             Ok(()), // case_19
             Ok(()), // case_31
+            Ok(()), // supplementary case s11 mixed versioning
         );
     }
 }

67b377cd6ec9f06f2ec190aac23c7d9121586426
Johannes Pein <johannes.pein@xitaso.com>
2026-07-24 09:50:15 +0200

    chore: add cwe 4.20 to asset update (#748)
    * chore: add cwe 4.20 to asset update
    
    * fix: use zip tool available on linux and mac

--- scripts/update/update_cwe.sh
+++ scripts/update/update_cwe.sh
@@ -9,4 +9,4 @@ REPO_ROOT="$(git rev-parse --show-toplevel 2>/dev/null)" || {
 cd "$REPO_ROOT"
 
 VERSION=${1:-latest}
-curl -fS https://cwe.mitre.org/data/xml/cwec_${VERSION}.xml.zip | zcat | xsltproc scripts/update/convert-cwe-to-csv.xslt - | (read -r header; sort -n > csaf-rs/assets/cwe/cwe_${header}.csv )
+curl -fS https://cwe.mitre.org/data/xml/cwec_${VERSION}.xml.zip | funzip | xsltproc scripts/update/convert-cwe-to-csv.xslt - | (read -r header; sort -n > csaf-rs/assets/cwe/cwe_${header}.csv )

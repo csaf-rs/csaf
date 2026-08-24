0f74f1684f6032d357a960c02fdf9ce2b9ae7ca3
Johannes Pein <johannes.pein@xitaso.com>
2026-08-24 15:37:42 +0200

    chore: fix some typos (#865)

--- csaf-rs/src/validation_result.rs
+++ csaf-rs/src/validation_result.rs
@@ -80,7 +80,7 @@ pub enum TestResult {
 pub enum TestFinding {
     /// An information indicates a failure in an informative test, which does not necessarily mean the document is invalid,
     /// but may provide insights into common mistakes or bad practices.
-    Infomation(TestFindingData),
+    Information(TestFindingData),
     /// A warning indicates a failure in a recommended test, which does not necessarily mean the document is invalid.
     /// However, it may indicate potential issues or areas for improvement in the document.
     Warning(TestFindingData),

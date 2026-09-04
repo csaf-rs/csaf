f99090038edce3619b6a7bbf778019001146cc7e
Boris Shek <boris.shek@xitaso.com>
2026-09-04 09:47:53 +0200

    feat: add tests 6.2.4 for CSAF 2.1
    * feat: add tests 6.2.4 for CSAF 2.1
    
    * refactor: improve test case naming in test_6_2_04
    
    * test: reuse CSAF 2.1 supplementary coverage for 6.2.4 in CSAF 2.0

--- csaf-rs/src/csaf2_1/testcases.generated.rs
+++ csaf-rs/src/csaf2_1/testcases.generated.rs
@@ -1632,7 +1632,15 @@ crate::macros::define_csaf_test!(
     crate ::schema::csaf2_1::schema::CommonSecurityAdvisoryFramework, version : "V2_1",
     cases : [(case_01, "01",
     "../csaf/csaf_2.1/test/validator/data/recommended/oasis_csaf_tc-csaf_2_1-2024-6-2-04-01.json",
-    "recommended/oasis_csaf_tc-csaf_2_1-2024-6-2-04-01.json")]
+    "recommended/oasis_csaf_tc-csaf_2_1-2024-6-2-04-01.json"), (case_s01, "s01",
+    "../type-generator/assets/tests/csaf_2.1/recommended/csaf-rs_csaf-csaf_2_1-6-2-04-s01.json",
+    "recommended/csaf-rs_csaf-csaf_2_1-6-2-04-s01.json"), (case_s02, "s02",
+    "../type-generator/assets/tests/csaf_2.1/recommended/csaf-rs_csaf-csaf_2_1-6-2-04-s02.json",
+    "recommended/csaf-rs_csaf-csaf_2_1-6-2-04-s02.json"), (case_s11, "s11",
+    "../type-generator/assets/tests/csaf_2.1/recommended/csaf-rs_csaf-csaf_2_1-6-2-04-s11.json",
+    "recommended/csaf-rs_csaf-csaf_2_1-6-2-04-s11.json"), (case_s12, "s12",
+    "../type-generator/assets/tests/csaf_2.1/recommended/csaf-rs_csaf-csaf_2_1-6-2-04-s12.json",
+    "recommended/csaf-rs_csaf-csaf_2_1-6-2-04-s12.json")]
 );
 crate::macros::define_csaf_test!(
     Test6_2_5, ValidatorForTest6_2_5, ExpectedResults_6_2_5, id : "6.2.5", doc_type :

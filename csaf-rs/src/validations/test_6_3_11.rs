5dcacce8162d7e024703ce0301248f7b15e7fdee
Johannes Pein <johannes.pein@xitaso.com>
2026-09-07 22:14:28 +0200

    fix: cr comment

--- csaf-rs/src/validations/test_6_3_11.rs
+++ csaf-rs/src/validations/test_6_3_11.rs
@@ -3,6 +3,7 @@ use crate::validation::{TestFinding, TestFindingData};
 
 /// To implement this test it is deemed sufficient that the value of name does not match the following regex:
 /// `^[vV][0-9].*$`. This function implements this trivial regex using regular string operations, which is significantly faster.
+/// It additionally captures the edge case of the branch name containing newline characters.
 fn is_version_with_v_indicator(version: &str) -> bool {
     let mut chars = version.chars();
     matches!(chars.next(), Some('v') | Some('V')) && matches!(chars.next(), Some(c) if c.is_ascii_digit())
@@ -57,6 +58,8 @@ mod tests {
     #[case("V4.2", true)]
     #[case("v1.0.0", true)]
     #[case("V1.0.0", true)]
+    #[case("v1\nalpha", true)]
+    #[case("V1\nalpha", true)]
     #[case("", false)]
     #[case("v", false)]
     #[case("V", false)]

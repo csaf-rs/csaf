140ca28ad45406cd54c5967ff04a1e557e1ed2e1
Johannes Pein <johannes.pein@xitaso.com>
2026-09-10 11:55:14 +0200

    fix: typo

--- scripts/json/validate_upstream_test_validity.sh
+++ scripts/json/validate_upstream_test_validity.sh
@@ -13,11 +13,11 @@ cargo build -p csaf-validator --release
 
 # Paths
 VALIDATOR_PATH="$REPO_ROOT/target/release/csaf-validator"
-CSAF_2_0_TESTCSAES_JSON="$REPO_ROOT/csaf/csaf_2.0/test/validator/data/testcases.json"
+CSAF_2_0_TESTCASES_JSON="$REPO_ROOT/csaf/csaf_2.0/test/validator/data/testcases.json"
 CSAF_2_1_TESTCASES_JSON="$REPO_ROOT/csaf/csaf_2.1/test/validator/data/testcases.json"
 
 echo "Checking upstream tests for CSAF 2.0..."
-python3 "$REPO_ROOT/scripts/json/validate_testcases_json_validity.py" "$VALIDATOR_PATH" "$CSAF_2_0_TESTCSAES_JSON" "2.0"
+python3 "$REPO_ROOT/scripts/json/validate_testcases_json_validity.py" "$VALIDATOR_PATH" "$CSAF_2_0_TESTCASES_JSON" "2.0"
 
 echo "Checking upstream tests for CSAF 2.1..."
 python3 "$REPO_ROOT/scripts/json/validate_testcases_json_validity.py" "$VALIDATOR_PATH" "$CSAF_2_1_TESTCASES_JSON" "2.1"

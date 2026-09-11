6db43b6fe982d615a1e2d27d265ef7220a2fdbf8
Johannes Pein <johannes.pein@xitaso.com>
2026-09-11 17:05:44 +0200

    chore: regen testcases setup

--- csaf-rs/src/csaf2_1/testcases.generated.rs
+++ csaf-rs/src/csaf2_1/testcases.generated.rs
@@ -2580,7 +2580,9 @@ crate::macros::define_csaf_test!(
     "../csaf/csaf_2.1/test/validator/data/informative/oasis_csaf_tc-csaf_2_1-2024-6-3-04-11.json",
     "informative/oasis_csaf_tc-csaf_2_1-2024-6-3-04-11.json"), (case_12, "12",
     "../csaf/csaf_2.1/test/validator/data/informative/oasis_csaf_tc-csaf_2_1-2024-6-3-04-12.json",
-    "informative/oasis_csaf_tc-csaf_2_1-2024-6-3-04-12.json")]
+    "informative/oasis_csaf_tc-csaf_2_1-2024-6-3-04-12.json"), (case_s11, "s11",
+    "../type-generator/assets/tests/csaf_2.1/informative/csaf-rs_csaf-csaf_2_1-6-3-04-s11.json",
+    "informative/csaf-rs_csaf-csaf_2_1-6-3-04-s11.json")]
 );
 crate::macros::define_csaf_test!(
     Test6_3_5, ValidatorForTest6_3_5, ExpectedResults_6_3_5, id : "6.3.5", doc_type :

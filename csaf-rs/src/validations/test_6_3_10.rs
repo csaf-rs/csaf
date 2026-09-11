7e227709ec4f402a4878b6f72a3d6a61130576bf
Johannes Pein <johannes.pein@xitaso.com>
2026-09-11 14:56:05 +0200

    feat: apply new test cases to 6.3.10

--- csaf-rs/src/validations/test_6_3_10.rs
+++ csaf-rs/src/validations/test_6_3_10.rs
@@ -41,18 +41,38 @@ mod tests {
 
     #[test]
     fn test_test_6_3_10() {
-        let case_01 = Err(vec![create_product_version_range_error(
+        let tree_with_product_version_range = Err(vec![create_product_version_range_error(
             "/product_tree/branches/0/branches/0/branches/0",
         )]);
 
+        let tree_with_parallel_product_version_range = Err(vec![
+            create_product_version_range_error("/product_tree/branches/0/branches/0/branches/0"),
+            create_product_version_range_error("/product_tree/branches/0/branches/0/branches/1"),
+        ]);
+
+        // Stacked product categories violate 6.1.57 on CSAF 2.1, making this test file invalid there
+        let tree_with_stacked_product_version_range = Err(vec![
+            create_product_version_range_error("/product_tree/branches/0/branches/0/branches/0"),
+            create_product_version_range_error("/product_tree/branches/0/branches/0/branches/0/branches/0"),
+        ]);
+
+        // Case 11: product tree without product version range
+        // Case S11: no product tree
+
         // Both CSAF 2.0 and 2.1 have 2 test cases
         TESTS_2_0.test_6_3_10.expect(ExpectedResults_2_0 {
-            case_01: case_01.clone(),
+            case_01: tree_with_product_version_range.clone(),
+            case_s01: tree_with_parallel_product_version_range.clone(),
+            case_s02: tree_with_stacked_product_version_range.clone(),
             case_11: Ok(()),
+            case_s11: Ok(()), // TODO #409 wasSkipped
         });
         TESTS_2_1.test_6_3_10.expect(ExpectedResults_2_1 {
-            case_01,
+            case_01: tree_with_product_version_range,
+            case_s01: tree_with_parallel_product_version_range,
+            case_s02: tree_with_stacked_product_version_range,
             case_11: Ok(()),
+            case_s11: Ok(()), // TODO #409 wasSkipped
         });
     }
 }

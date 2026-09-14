ab47bc7dddf750bf387ed52e58ee4a2c7d252f9f
Johannes Pein <johannes.pein@xitaso.com>
2026-09-14 16:24:41 +0200

    fix: post merge fixes

--- csaf-rs/src/csaf2_1/validation.rs
+++ csaf-rs/src/csaf2_1/validation.rs
@@ -114,7 +114,7 @@ impl TryFrom<&str> for Preset {
             PRESET_NAME_CONSISTENT_DATE_TIMES => Ok(Preset::ConsistentDateTimes),
             PRESET_NAME_SSVC => Ok(Preset::Ssvc),
             PRESET_NAME_EXTENSIONS => Ok(Preset::Extensions),
-            PRESET_NAME_EXTENSION_EXIST => Ok(Preset::ExtensionsExist),
+            PRESET_NAME_EXTENSIONS_EXIST => Ok(Preset::ExtensionsExist),
             _ => Err(CsafError::InvalidPreset {
                 preset: value.to_string(),
             }),

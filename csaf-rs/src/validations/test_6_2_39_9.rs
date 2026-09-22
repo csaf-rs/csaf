c37f22c6da714abe91b7f3ba64cfc5b2d3906038
Boris Shek <boris.shek@xitaso.com>
2026-09-22 20:28:43 +0200

    feat: implement rule 6.2.39.9 for CSAF 2.1

---
+++ csaf-rs/src/validations/test_6_2_39_9.rs
@@ -0,0 +1,63 @@
+use crate::csaf::types::csaf_document_category::CsafDocumentCategory;
+use crate::csaf_traits::{CsafTrait, DistributionTrait, DocumentTrait, TlpTrait};
+use crate::schema::csaf2_1::schema::LabelOfTlp;
+use crate::validation::{TestFinding, TestFindingData};
+use crate::validations::utils::document_category_test_config::DocumentCategoryTestConfig;
+
+/// 6.2.39.9 TLP-Label for Vulnerability Report
+///
+/// This test only applies to documents with `/document/category` with value
+/// `csaf_vulnerability_report`.
+///
+/// It SHALL be tested that `/document/distribution/tlp/label` is set to `AMBER`.
+pub fn test_6_2_39_9_tlp_label_for_vulnerability_report(doc: &impl CsafTrait) -> Result<(), Vec<TestFinding>> {
+    let document = doc.get_document();
+    let document_category = document.get_category();
+
+    if !PROFILE_TEST_CONFIG.matches_category_with_csaf_version(document.get_csaf_version(), &document_category) {
+        return Ok(());
+    }
+
+    let distribution = document.get_distribution_21().map_err(|e| vec![e])?;
+
+    if distribution.get_tlp_21().map_err(|e| vec![e])?.get_label() != LabelOfTlp::Amber {
+        return Err(vec![create_non_amber_tlp_label_warning()]);
+    }
+
+    Ok(())
+}
+
+fn create_non_amber_tlp_label_warning() -> TestFinding {
+    TestFinding::Warning(TestFindingData {
+        message: "The vulnerability report uses a TLP label other than the recommended value `AMBER`.".to_string(),
+        instance_path: "/document/distribution/tlp/label".to_string(),
+    })
+}
+
+const PROFILE_TEST_CONFIG: DocumentCategoryTestConfig =
+    DocumentCategoryTestConfig::new().csaf21(&[CsafDocumentCategory::CsafVulnerabilityReport]);
+
+crate::test_validation::impl_validator!(
+    csaf2_1,
+    ValidatorForTest6_2_39_9,
+    test_6_2_39_9_tlp_label_for_vulnerability_report
+);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::csaf2_1::testcases::ExpectedResults_6_2_39_9 as ExpectedResults;
+    use crate::csaf2_1::testcases::TESTS_2_1;
+
+    #[test]
+    fn test_test_6_2_39_9() {
+        let non_amber_tlp_label = Err(vec![create_non_amber_tlp_label_warning()]);
+
+        // Case 01: TLP label is not AMBER
+        TESTS_2_1.test_6_2_39_9.expect(ExpectedResults {
+            case_01: non_amber_tlp_label,
+            // Case 11: TLP label is AMBER
+            case_11: Ok(()),
+        });
+    }
+}

42e07578f4ebc638287fc489d95ee3474b1fffeb
Boris Shek <boris.shek@xitaso.com>
2026-09-24 09:22:10 +0200

    feat: implement rule 6.2.39.7 for CSAF 2.1 (#1087)

--- csaf-rs/src/validations/mod.rs
+++ csaf-rs/src/validations/mod.rs
@@ -119,6 +119,7 @@ pub mod test_6_2_38;
 pub mod test_6_2_39_2;
 pub mod test_6_2_39_3;
 pub mod test_6_2_39_4;
+pub mod test_6_2_39_7;
 pub mod test_6_2_39_8;
 pub mod test_6_2_39_9;
 pub mod test_6_2_41;

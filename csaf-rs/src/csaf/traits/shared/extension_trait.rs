a95b22d4496fc5c9189d1ed9c96833b00061f629
Johannes Pein <johannes.pein@xitaso.com>
2026-09-25 14:28:38 +0200

    refactor: rename to `get_raw`

--- csaf-rs/src/csaf/traits/shared/extension_trait.rs
+++ csaf-rs/src/csaf/traits/shared/extension_trait.rs
@@ -3,17 +3,17 @@ use crate::schema::csaf2_1::schema::ExtensionsT as Extensions21;
 use serde_json::{Map, Value};
 
 pub trait ExtensionsTrait {
-    fn get_content(&self) -> &Vec<Map<String, Value>>;
+    fn get_raw(&self) -> &Vec<Map<String, Value>>;
 }
 
 impl ExtensionsTrait for NotPresentInCsaf20 {
-    fn get_content(&self) -> &Vec<Map<String, Value>> {
+    fn get_raw(&self) -> &Vec<Map<String, Value>> {
         self.into_any()
     }
 }
 
 impl ExtensionsTrait for Extensions21 {
-    fn get_content(&self) -> &Vec<Map<String, Value>> {
+    fn get_raw(&self) -> &Vec<Map<String, Value>> {
         &self.0
     }
 }

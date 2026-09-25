3fb0621558fa5bc640ff17af67336481cc1cb64f
Johannes Pein <johannes.pein@xitaso.com>
2026-09-25 09:00:45 +0200

    feat: add test 6.2.39.5 for CSAF 2.1

---
+++ csaf-rs/src/validations/test_6_2_39_5.rs
@@ -0,0 +1,94 @@
+use serde_json::Value;
+
+use crate::csaf::types::csaf_document_category::CsafDocumentCategory;
+use crate::csaf_traits::CsafVersion;
+use crate::validation::{TestFinding, TestFindingData};
+use crate::validations::utils::document_category_test_config::DocumentCategoryTestConfig;
+use crate::validations::utils::raw_json::property_exists;
+
+fn create_extension_warning(doc_category: &CsafDocumentCategory, instance_path: &str) -> TestFinding {
+    TestFinding::Warning(TestFindingData {
+        message: format!(
+            "The document contains a CSAF Extension, which is not recommended for category `{doc_category}`."
+        ),
+        instance_path: instance_path.to_string(),
+    })
+}
+
+/// 6.2.39.5 Extension in Superseded or Withdrawn Document
+///
+/// This test only applies to documents with `/document/category` with value `csaf_withdrawn` or `csaf_superseded`.
+///
+/// It SHALL be tested that the document does not contain an extension.
+/// The relevant paths for this test are:
+/// - `/document/x_extensions`
+/// - `/x_extensions`
+pub fn test_6_2_39_5_extension_in_superseded_or_withdrawn_document(json: &Value) -> Result<(), Vec<TestFinding>> {
+    // read category
+    let doc_category = match json.pointer("/document/category") {
+        Some(Value::String(cat)) => CsafDocumentCategory::from(cat.as_str()),
+        _ => return Ok(()),
+    };
+
+    // CSAF 2.1 is hard-coded here, as this test did not exist in CSAF 2.0, and extensions are a CSAF 2.1 feature.
+    if !PROFILE_TEST_CONFIG.matches_category_with_csaf_version(CsafVersion::X21, &doc_category) {
+        return Ok(());
+    }
+
+    let mut errors: Option<Vec<TestFinding>> = None;
+
+    // check if the extension path exists
+    let mut check_document_extension = |instance_path: &str| {
+        if property_exists(instance_path, json) {
+            errors
+                .get_or_insert_default()
+                .push(create_extension_warning(&doc_category, instance_path));
+        }
+    };
+
+    check_document_extension("/document/x_extensions");
+    check_document_extension("/x_extensions");
+
+    errors.map_or(Ok(()), Err)
+}
+
+crate::test_validation::impl_raw_json_validator!(
+    csaf2_1,
+    ValidatorForTest6_2_39_5,
+    test_6_2_39_5_extension_in_superseded_or_withdrawn_document
+);
+
+const PROFILE_TEST_CONFIG: DocumentCategoryTestConfig = DocumentCategoryTestConfig::new().csaf21(&[
+    CsafDocumentCategory::CsafWithdrawn,
+    CsafDocumentCategory::CsafSuperseded,
+]);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::csaf2_1::testcases::ExpectedResults_6_2_39_5 as ExpectedResults;
+    use crate::csaf2_1::testcases::TESTS_2_1;
+
+    #[test]
+    fn test_test_6_2_39_5() {
+        let root_extension_withdrawn = Err(vec![create_extension_warning(
+            &CsafDocumentCategory::CsafWithdrawn,
+            "/x_extensions",
+        )]);
+
+        let root_extension_superseded = Err(vec![create_extension_warning(
+            &CsafDocumentCategory::CsafSuperseded,
+            "/x_extensions",
+        )]);
+
+        // Case 11: withdrawn document without extension
+        // Case 12: superseded document without extension
+
+        TESTS_2_1.test_6_2_39_5.expect(ExpectedResults {
+            case_01: root_extension_superseded,
+            case_02: root_extension_withdrawn,
+            case_11: Ok(()),
+            case_12: Ok(()),
+        });
+    }
+}
